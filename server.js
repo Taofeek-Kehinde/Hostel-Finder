bc86ec012875735bacfdffa0c38b5bdb449d41dd
Taofeek Kehinde <Ktaofeek015@gmal.com>
2025-12-02 14:33:05 +0100

    More Erros

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ import 'dotenv/config';
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+app.use(cors({ origin: '*' }));
 // Middleware
 app.use(express.json());
 app.use(cors());
@@ -614,3 +614,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}`);
 });
+
+
